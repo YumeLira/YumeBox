c5aedb7d3e6ee673f3059e6d6154ed0c17e87067
梦璃酱 <Miao@YumeYuka.plus>
2026-01-19 01:24:29 +0800

    Fix root Gradle plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,14 @@
-val agpVersion = "9.0.0"
-val kotlinVersion = "2.3.0"
-val composePluginVersion = "1.9.3"
-val kspVersion = "2.3.3"
-val aboutLibrariesVersion = "13.2.1"
-val googleServicesVersion = "4.4.4"
-val crashlyticsVersion = "3.0.6"
-
 plugins {
-    id("com.android.application") version agpVersion apply false
-    id("com.android.library") version agpVersion apply false
-    kotlin("android") version kotlinVersion apply false
-    kotlin("multiplatform") version kotlinVersion apply false
-    kotlin("plugin.serialization") version kotlinVersion apply false
-    kotlin("plugin.compose") version kotlinVersion apply false
-    id("org.jetbrains.compose") version composePluginVersion apply false
-    id("com.google.devtools.ksp") version kspVersion apply false
-    id("com.mikepenz.aboutlibraries.plugin") version aboutLibrariesVersion apply false
-    id("com.google.gms.google-services") version googleServicesVersion apply false
-    id("com.google.firebase.crashlytics") version crashlyticsVersion apply false
+    id("com.android.application") version "9.0.0" apply false
+    id("com.android.library") version "9.0.0" apply false
+    kotlin("android") version "2.3.0" apply false
+    kotlin("multiplatform") version "2.3.0" apply false
+    kotlin("plugin.serialization") version "2.3.0" apply false
+    kotlin("plugin.compose") version "2.3.0" apply false
+    id("org.jetbrains.compose") version "1.9.3" apply false
+    id("com.google.devtools.ksp") version "2.3.3" apply false
+    id("com.mikepenz.aboutlibraries.plugin") version "13.2.1" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.firebase.crashlytics") version "3.0.6" apply false
     id("dev.oom-wg.purejoy.fyl.fytxt") version "+" apply false
 }
