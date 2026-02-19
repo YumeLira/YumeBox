f73dbfc2de4b1ab25ae4c1c048f653cc40f328e7
YumeYuka <YumeYuka@proton.me>
2026-02-19 22:34:09 +0800

    bump(version): update project version to 0.3.8

--- app/src/main/kotlin/com/github/yumelira/yumebox/data/store/AppSettingsStore.kt
+++ app/src/main/kotlin/com/github/yumelira/yumebox/data/store/AppSettingsStore.kt
@@ -42,9 +42,9 @@ class AppSettingsStorage(externalMmkv: MMKV) : MMKVPreference(externalMmkv = ext
     val bottomBarAutoHide by boolFlow(true)
     val iconWithSelectedLabel by boolFlow(true)
 
-    val oneWord by strFlow("もうサヨウナラ，もう終わらなきゃ")
+    val oneWord by strFlow("少しだけあなたに届く？会いたいでも会えないから，")
 
-    val oneWordAuthor by strFlow("恋時雨")
+    val oneWordAuthor by strFlow("夏の雪")
 
     val customUserAgent by strFlow("")
 }
